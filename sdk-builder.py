cb56ee793e34ce2b887f5bd37c6f93f2c7ceacbf
Thany <hossein.noroozpour@gmail.com>
2022-08-27 04:47:48 +0100

    for supporting linux

--- sdk-builder.py
+++ sdk-builder.py
@@ -13,21 +13,29 @@ platform_name = platform.platform().lower()
 
 platforms_settings = dict()
 
+PLATFORM_WINDOWS_KEY = 'windows'
+PLATFORM_LINUX_KEY = 'linux'
+PLATFORM_ANDROID_KEY = 'android'
+PLATFORM_MACOS_KEY = 'macos'
+PLATFORM_IOS_KEY = 'ios'
 CMAKE_PATH_KEY = 'cmake-path'
 CMAKE_ARGS_KEY = 'cmake-args'
+PLATFORM_BUILD_T1_PATH_KEY = 'platform-build-t1-path'
 
-if 'windows' in platform_name:
+if PLATFORM_WINDOWS_KEY in platform_name:
     print('Windows platform added.')
-    platforms_settings['windows'] = {
+    platforms_settings[PLATFORM_WINDOWS_KEY] = {
         CMAKE_PATH_KEY: 'cmake',
         CMAKE_ARGS_KEY: [],
+        PLATFORM_BUILD_T1_PATH_KEY: 'Realse',
     }
 
-if 'linux' in platform_name:
+if PLATFORM_LINUX_KEY in platform_name:
     print('Linux platform added.')
-    platforms_settings['linux'] = {
+    platforms_settings[PLATFORM_LINUX_KEY] = {
         CMAKE_PATH_KEY: 'cmake',
         CMAKE_ARGS_KEY: [],
+        PLATFORM_BUILD_T1_PATH_KEY: '',
     }
 
 android_ndk = os.environ.get('ANDROID_NDK')
@@ -37,24 +45,27 @@ if android_ndk is not None and android_sdk is not None:
     android_ndk = pathlib.Path(android_ndk)
     android_sdk = pathlib.Path(android_sdk)
     print('Android platform added.')
-    platforms_settings['android'] = {
+    platforms_settings[PLATFORM_WINDOWS_KEY] = {
         CMAKE_PATH_KEY: android_sdk / 'cmake' / '3.22.1' / 'bin' / 'cmake',
         CMAKE_ARGS_KEY: [
             '-DCMAKE_TOOLCHAIN_FILE=' +
             str(android_ndk) + '/build/cmake/android.toolchain.cmake',
             '-DANDROID_STL=c++_shared'
         ],
+        PLATFORM_BUILD_T1_PATH_KEY: '',
     }
 
-if 'macos' in platform_name:
+if PLATFORM_MACOS_KEY in platform_name:
     print('MacOS & iOS platforms added.')
-    platforms_settings['macos'] = {
+    platforms_settings[PLATFORM_MACOS_KEY] = {
         CMAKE_PATH_KEY: 'cmake',
         CMAKE_ARGS_KEY: [],
+        PLATFORM_BUILD_T1_PATH_KEY: '',
     }
-    platforms_settings['ios'] = {
+    platforms_settings[PLATFORM_IOS_KEY] = {
         CMAKE_PATH_KEY: 'cmake',
         CMAKE_ARGS_KEY: [],
+        PLATFORM_BUILD_T1_PATH_KEY: '',
     }
 
 curr_dir = pathlib.Path(__file__).parent
@@ -105,7 +116,7 @@ def build_t1(root_dir, src_dir, after_build_fn, additional_cmake_args = []):
             '--parallel', str(multiprocessing.cpu_count())]).returncode:
             logging.error('Cmake build failed for platform: %s, source: %s', plt, src_dir)
             continue
-        after_build_fn(sdk_dir / plt / 'lib', bld_dir / 'Release')
+        after_build_fn(sdk_dir / plt / 'lib', bld_dir / settings[PLATFORM_BUILD_T1_PATH_KEY])
 
 
 sdl2_str = 'sdl2'
@@ -121,8 +132,11 @@ sdl2_src_dir = sdl2_root_dir / sdl2_extracted_dir_name
 
 
 def sdl2_after_build(sdk_lib_path, built_libs_path):
-    shutil.copy(built_libs_path / 'SDL2-static.lib', sdk_lib_path)
-    shutil.copy(built_libs_path / 'SDL2main.lib', sdk_lib_path)
+    if PLATFORM_WINDOWS_KEY in platforms_settings:
+        shutil.copy(built_libs_path / 'SDL2-static.lib', sdk_lib_path)
+        shutil.copy(built_libs_path / 'SDL2main.lib', sdk_lib_path)
+    if PLATFORM_LINUX_KEY in platforms_settings:
+        print('No need for sdl in linux.')
 
 
 build_t1(sdl2_root_dir, sdl2_src_dir, sdl2_after_build, ['-DSDL_LIBC=ON'])
@@ -140,7 +154,10 @@ openal_src_dir = openal_root_dir / openal_extracted_dir_name
 
 
 def openal_after_build(sdk_lib_path, built_libs_path):
-    shutil.copy(built_libs_path / 'OpenAL32.lib', sdk_lib_path)
+    if PLATFORM_WINDOWS_KEY in platforms_settings:
+        shutil.copy(built_libs_path / 'OpenAL32.lib', sdk_lib_path)
+    if PLATFORM_LINUX_KEY in platforms_settings:
+        print('No need for OpenAL in linux.')
 
 
 build_t1(openal_root_dir, openal_src_dir, openal_after_build)
